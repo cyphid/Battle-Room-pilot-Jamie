1892683426674c7f0aef860a6d61ecde3c9ed857
cyphid <146806761+cyphid@users.noreply.github.com>
2023-12-05 05:52:35 +0000

    2023-11-28 submission

---
+++ GameStateClasses.py
@@ -0,0 +1,175 @@
+import typing
+from dataclasses import dataclass
+
+
+@dataclass(order=False, frozen=True)
+class Position:
+  x: int
+  y: int
+
+  def __str__(self) -> str:
+    return f"({self.x}, {self.y})"
+
+  def __add__(self, other):
+    if isinstance(other, Position):
+      return Position(self.x + other.x, self.y + other.y)
+    elif isinstance(other, tuple):
+      return Position(self.x + other[0], self.y + other[1])
+    else:
+      raise TypeError(
+          f"unsupported operand type(s) for +: 'Position' and '{type(other)}'")
+
+  def __sub__(self, other):
+    if isinstance(other, Position):
+      return Position(self.x - other.x, self.y - other.y)
+    elif isinstance(other, tuple):
+      return Position(self.x - other[0], self.y - other[1])
+    else:
+      raise TypeError(
+          f"unsupported operand type(s) for -: 'Position' and '{type(other)}'")
+
+  # returns the manhatten distance between two positions
+  def __abs__(self):
+    return abs(self.x) + abs(self.y)
+
+
+@dataclass(order=False, frozen=True)
+class Customisations:
+  color: str
+  head: str
+  tail: str
+
+
+@dataclass(order=False)
+class Snake:
+
+  id: str
+  name: str
+  health: int
+  body: typing.List[Position]
+  head: Position
+  length: int
+  shout: str
+  customisations: Customisations
+
+  def __init__(self, snake_dict) -> None:
+    self.id: str = snake_dict["id"]
+    self.name: str = snake_dict["name"]
+    self.health: int = snake_dict["health"]
+    self.body: typing.List[Position] = [
+        Position(**i) for i in snake_dict["body"]
+    ]
+    self.head: Position = Position(**snake_dict["head"])
+    self.length: int = snake_dict["length"]
+    self.shout: str = snake_dict["shout"]
+    self.customisations: Customisations = Customisations(
+        **snake_dict["customizations"])
+
+  def __eq__(self, other):
+    if isinstance(other, Snake):
+      return self.id == other.id
+    else:
+      raise TypeError(
+          f"unsupported operand type(s) for ==: 'Snake' and '{type(other)}'")
+
+
+@dataclass(order=False)
+class Board:
+
+  height: int
+  width: int
+  food: typing.List[Position]
+  snakes: typing.List[Snake]
+  hazards: typing.List[Position]
+
+  def __init__(self, board_dict) -> None:
+    self.height: int = board_dict["height"]
+    self.width: int = board_dict["width"]
+    self.food: typing.List[Position] = [
+        Position(**i) for i in board_dict["food"]
+    ]
+    self.snakes: typing.List[Snake] = [Snake(i) for i in board_dict["snakes"]]
+    self.hazards: typing.List[Position] = [
+        Position(**i) for i in board_dict["hazards"]
+    ]
+
+
+@dataclass(order=False)
+class Squad:
+
+  allow_body_collisions: bool
+  shared_elimination: bool
+  shared_health: bool
+  shared_length: bool
+
+  def __init__(self, squad_dict) -> None:
+    self.allow_body_collisions: bool = squad_dict["allowBodyCollisions"]
+    self.shared_elimination: bool = squad_dict["sharedElimination"]
+    self.shared_health: bool = squad_dict["sharedHealth"]
+    self.shared_length: bool = squad_dict["sharedLength"]
+
+
+@dataclass(order=False)
+class Royale:
+  shrink_every_n_turns: int
+
+  def __init__(self, royale_dict) -> None:
+    self.shrink_every_n_turns: int = royale_dict["shrinkEveryNTurns"]
+
+
+@dataclass(order=False)
+class Settings:
+  food_spawn_chance: int
+  minimum_food: int
+  hazard_damage_per_turn: int
+  royale: Royale
+  squad: Squad
+
+  def __init__(self, settings_dict) -> None:
+    self.food_spawn_chance: int = settings_dict["foodSpawnChance"]
+    self.minimum_food: int = settings_dict["minimumFood"]
+    self.hazard_damage_per_turn: int = settings_dict["hazardDamagePerTurn"]
+    self.royale: Royale = Royale(settings_dict["royale"])
+    self.squad: Squad = Squad(settings_dict["squad"])
+
+
+@dataclass(order=False)
+class Ruleset:
+  name: str
+  version: str
+  settings: Settings
+
+  def __init__(self, ruleset_dict) -> None:
+    self.name: str = ruleset_dict["name"]
+    self.version: str = ruleset_dict["version"]
+    self.settings: Settings = Settings(ruleset_dict["settings"])
+
+
+@dataclass(order=False)
+class Game:
+  id: str
+  ruleset: Ruleset
+  map: str
+  timeout: int
+  source: str
+
+  def __init__(self, game_dict: typing.Dict) -> None:
+    self.id: str = game_dict["id"]
+    self.ruleset: Ruleset = Ruleset(game_dict["ruleset"])
+    self.map: str = game_dict["map"]
+    self.timeout: int = game_dict["timeout"]
+    self.source: str = game_dict["source"]
+
+
+@dataclass(order=False)
+class GameState:
+  game: Game
+  board: Board
+  you: Snake
+  turn: int
+
+  def __init__(self, current_game_state) -> None:
+    self.game: Game = Game(current_game_state["game"])
+    self.board: Board = Board(current_game_state["board"])
+    self.you: Snake = Snake(current_game_state["you"])
+    self.turn: int = current_game_state["turn"]
--- main.py
+++ main.py
@@ -12,7 +12,8 @@
 
 import random
 import typing
-
+from GameStateClasses import (Position, GameState, Game, Ruleset, Board,
+Snake, Customisations)
 
 # info is called when you create your Battlesnake on play.battlesnake.com
 # and controls your Battlesnake's appearance
@@ -23,12 +24,11 @@ def info() -> typing.Dict:
     return {
         "apiversion": "1",
         "author": "",  # TODO: Your Battlesnake Username
-        "color": "#337453",  # TODO: Choose color
-        "head": "default",  # TODO: Choose head
-        "tail": "default",  # TODO: Choose tail
+        "color": "#1dbfbb",  # TODO: Choose color
+        "head": "gamer",  # TODO: Choose head
+        "tail": "coffee",  # TODO: Choose tail
     }
-
-
+ 
 # start is called when your Battlesnake begins a game
 def start(game_state: typing.Dict):
     print("GAME START")
@@ -38,53 +38,101 @@ def start(game_state: typing.Dict):
 def end(game_state: typing.Dict):
     print("GAME OVER\n")
 
+def closest_food(game_state: GameState, origin: Position):
+  food_list = game_state.board.food
+  closest_food_distance = 100000
+  for food in food_list:
+    diff_x = abs(game_state.you.head.x - food.x)
+    diff_y = abs(game_state.you.head.y - food.y)
+    total_distance = diff_x + diff_y
+    if total_distance < closest_food_distance:
+      closest_food_distance = total_distance
+  return closest_food_distance
+    
+  
 
 # move is called on every turn and returns your next move
 # Valid moves are "up", "down", "left", or "right"
 # See https://docs.battlesnake.com/api/example-move for available data
 def move(game_state: typing.Dict) -> typing.Dict:
-
+    game_state = GameState(game_state)
     is_move_safe = {
       "up": True, 
       "down": True, 
       "left": True, 
       "right": True
     }
-
+    
     # We've included code to prevent your Battlesnake from moving backwards
-    my_head = game_state["you"]["body"][0]  # Coordinates of your head
-    my_neck = game_state["you"]["body"][1]  # Coordinates of your "neck"
+    my_head = game_state.you.body[0]  # Coordinates of your head
+    my_neck = game_state.you.body[1]  # Coordinates of your "neck"
 
-    if my_neck["x"] < my_head["x"]:  # Neck is left of head, don't move left
+    if my_neck.x < my_head.x:  # Neck is left of head, don't move left
         is_move_safe["left"] = False
 
-    elif my_neck["x"] > my_head["x"]:  # Neck is right of head, don't move right
+    elif my_neck.x > my_head.x:  # Neck is right of head, don't move right
         is_move_safe["right"] = False
 
-    elif my_neck["y"] < my_head["y"]:  # Neck is below head, don't move down
+    elif my_neck.y < my_head.y:  # Neck is below head, don't move down
         is_move_safe["down"] = False
 
-    elif my_neck["y"] > my_head["y"]:  # Neck is above head, don't move up
+    elif my_neck.y > my_head.y:  # Neck is above head, don't move up
         is_move_safe["up"] = False
 
     # TODO: Step 1 - Prevent your Battlesnake from moving out of bounds
-    # board_width = game_state['board']['width']
-    # board_height = game_state['board']['height']
 
-    # TODO: Step 2 - Prevent your Battlesnake from colliding with itself
-    # my_body = game_state['you']['body']
+    Board_width = game_state.board.width
+    Board_height = game_state.board.height
+    # we can remove the variables Board_width and Board_height
+    if my_head.x == 0:
+        is_move_safe["left"] = False
+    if my_head.x == Board_width - 1:
+       is_move_safe["right"] = False
+    if my_head.y == 0: 
+      is_move_safe["down"] = False
+    if my_head.y == Board_height - 1:
+      is_move_safe["up"] = False
+    
+        
+  
 
-    # TODO: Step 3 - Prevent your Battlesnake from colliding with other Battlesnakes
-    # opponents = game_state['board']['snakes']
+    # DONE: Step 2 - Prevent your Battlesnake from colliding with itself
+    my_snake = game_state.you
+    my_snake_head = my_snake.head
 
+    for snake in game_state.board.snakes:
+      for segment in snake.body:
+        if my_snake_head.x + 1 == segment.x and my_snake_head.y == segment.y:
+          is_move_safe["right"] = False
+        if my_snake_head.x - 1 == segment.x and my_snake_head.y == segment.y:
+          is_move_safe["left"] = False
+        if my_snake_head.y + 1 == segment.y and my_snake_head.x == segment.x:
+          is_move_safe["up"] = False
+        if my_snake_head.y - 1 == segment.y and my_snake_head.x == segment.x:
+          is_move_safe["down"] = False
+
+  
+    # TODO: Step 3 - Find Food
+    # dist = []
+  
+    # dist.append(closest_food(game_state, my_head + (0,1)  ))
+    # dist.append(closest_food(game_state, my_head + (0,-1) ))
+    # dist.append(closest_food(game_state, my_head + (1,0) ))
+    # dist.append(closest_food(game_state, my_head + (-1,0) ))
+
+    # min_dist = 1000000
+    # for i in dist:
+    #   if i < min_dist:
+    #     min_dist = i
+    
     # Are there any safe moves left?
     safe_moves = []
     for move, isSafe in is_move_safe.items():
         if isSafe:
             safe_moves.append(move)
-
+    
     if len(safe_moves) == 0:
-        print(f"MOVE {game_state['turn']}: No safe moves detected! Moving down")
+        print(f"MOVE {game_state.turn}: No safe moves detected! Moving down")
         return {"move": "down"}
 
     # Choose a random move from the safe ones
@@ -93,7 +141,7 @@ def move(game_state: typing.Dict) -> typing.Dict:
     # TODO: Step 4 - Move towards food instead of random, to regain health and survive longer
     # food = game_state['board']['food']
 
-    print(f"MOVE {game_state['turn']}: {next_move}")
+    # print(f"MOVE {game_state.turn}: {next_move}")
     return {"move": next_move}
 
 
